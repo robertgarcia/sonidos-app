9c6d700f13c767edfc5d205ca4998a9b8b780581
Roberto D. Garcia <robertdavid.mgt@gmail.com>
2019-09-03 09:41:20 -0600

    función de eliminar items de la lista

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -11,22 +11,49 @@ import { Animal } from '../../interfaces/animal.interface';
 export class HomePage {
 
   animales: Animal[] = [];
+  audio = new Audio();
+  audioTiempo: any;
+
   constructor() {
     this.animales = ANIMALES.slice(0); // Creamos un clon del objeto
   }
 
   reproducir(obj: Animal) {
-    console.log(obj);
-    const audio = new Audio();
-    audio.src = obj.audio;
-    audio.load();
-    audio.play();
+
+    this.pausar_audio(obj);
+
+    if (obj.reproduciendo) {
+      obj.reproduciendo = false;
+      return;
+    }
+
+    this.audio.src = obj.audio;
+    this.audio.load();
+    this.audio.play();
     obj.reproduciendo = true;
     obj.texto = 'Reproduciendo...';
     setTimeout(() => {
       obj.reproduciendo = false;
       obj.texto = 'Detenido...';
     }, obj.duracion * 1000 );
+  }
+
+  private pausar_audio( animalSel: Animal ) {
+    clearTimeout( this.audioTiempo );
+    this.audio.pause();
+    this.audio.currentTime = 0;
+
+    for (const animal of this.animales) {
+      if ( animal.nombre !== animalSel.nombre ) {
+        animal.reproduciendo = false;
+        animal.texto = 'Detenido...';
+      }
+    }
+  }
+
+  borrar(i: number) {
+    console.log(i);
+    this.animales.splice(i, 1);
   }
 
 }
